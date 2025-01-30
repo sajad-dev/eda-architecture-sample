21d2e0b793328bbaea089f2c92b1c9c641e0623d
sajad-dev <prj.sajad85@gmail.com>
2025-01-30 15:17:42 +0330

    Connect with pusher

--- go.mod
+++ go.mod
@@ -5,4 +5,7 @@ go 1.22.10
 require (
 	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/pusher/pusher-http-go v3.0.0+incompatible // indirect
+	golang.org/x/crypto v0.32.0 // indirect
+	golang.org/x/sys v0.29.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,71 +3,66 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
-	"os"
+	"net/http/httputil"
 
-	"github.com/gorilla/websocket"
-	"github.com/joho/godotenv"
+	"github.com/pusher/pusher-http-go"
 )
 
-type MsgStruct struct {
+type loggingTransport struct {
+	Transport http.RoundTripper
+}
+
+func (t *loggingTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	requestDump, err := httputil.DumpRequestOut(req, true)
+	if err != nil {
+		log.Println("Error dumping request:", err)
+	} else {
+		log.Println("REQUEST:")
+		log.Println(string(requestDump))
+	}
+	resp, err := t.Transport.RoundTrip(req)
+	return resp, err
+}
+
+var client = pusher.Client{
+	AppId:  "local",
+	Secret: "3ueVhNtuicMeJpYq",
+	Key:    "d6kAd89bMqDrLrFh",
+	Secure: false,
+	Host:   "127.0.0.1:8081",
+	HttpClient: &http.Client{
+		Transport: &loggingTransport{
+			Transport: http.DefaultTransport,
+		},
+	},
+}
+
+type Body struct {
 	Message string `json:"message"`
 }
 
-func handler(msg chan string, handlerFunc func(http.ResponseWriter, *http.Request, chan string)) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		handlerFunc(w, r, msg)
+func handelFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+	
+	var body Body
+	json.NewDecoder(r.Body).Decode(&body)
+	
+	err := client.Trigger("test", "publish", body.Message)
+	if err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		fmt.Println("Message sent successfully!")
 	}
-}
-
-func controller(w http.ResponseWriter, r *http.Request, msg chan string) {
-    w.Header().Set("Content-Type", "application/json")
-    w.Header().Set("Access-Control-Allow-Origin", "*")
-    w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
-    w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-
-    if r.Method == http.MethodOptions {
-        w.WriteHeader(http.StatusOK)
-        return
-    }
-
-    var message MsgStruct
-    if err := json.NewDecoder(r.Body).Decode(&message); err != nil {
-        http.Error(w, "Invalid JSON", http.StatusBadRequest)
-        return
-    }
-    msg <- message.Message
-    w.Write([]byte("ok"))
+	w.Write([]byte("h"))
 }
 
 func main() {
-	godotenv.Load(".env")
-	
-	url := fmt.Sprintf("ws://%s:%s/%s",os.Getenv("IP"),os.Getenv("WEBSOCKET_PORT"),os.Getenv("PUBLIC_KEY"))
-	headers := http.Header{}
-	headers.Add("secret_key",os.Getenv("SECRET_KEY"))
 
-	conn, _, err := websocket.DefaultDialer.Dial(url, headers)
-	if err != nil {
-		fmt.Println("Error connecting to WebSocket:", err)
-	}
+	http.HandleFunc("/send", handelFunc)
+	http.ListenAndServe(":8000", nil)
 
-	message := make(chan string)
-
-	go func() {
-		for {
-
-			select {
-			case msg := <-message:
-				err = conn.WriteMessage(websocket.TextMessage, []byte(msg))
-				if err != nil {
-					fmt.Println("Error sending message:", err)
-				}
-
-			}
-		}
-	}()
-
-	http.HandleFunc("/send", handler(message, controller))
-	http.ListenAndServe(fmt.Sprintf(":%s",os.Getenv("WEBSERVER_PORT")), nil)
 }
